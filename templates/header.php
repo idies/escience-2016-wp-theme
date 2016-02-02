2c52e3a996b2d93277f6653007fdc5ea6d432882
bonbons0220 <bsouter@jhu.edu>
2016-02-02 15:25:14 -0500

    added footer with copyright and social links

--- templates/header.php
+++ templates/header.php
@@ -5,13 +5,8 @@
   </div>
   <div class="container">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+	<a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+	<div class="bookmark navmenu"><a href="#navmenu"><i class="fa fa-bars fa-2x"></i></a></div>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
